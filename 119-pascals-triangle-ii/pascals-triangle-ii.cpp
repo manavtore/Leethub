1205bbdf9ad5a4a282215354c54998c4c76fded1
manavtore <113475112+manavtore@users.noreply.github.com>
2023-08-29 23:10:58 +0530

    Time: 3 ms (36.31%) | Memory: 6.5 MB (43.86%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/pascals-triangle-ii.cpp
@@ -2,23 +2,16 @@ class Solution {
 public:
     vector<int> getRow(int rowIndex) {
         vector<int> ans;
-        int res;
-        for(int i=0;i<=rowIndex;i++){
-            
-        res=returnelement(rowIndex,i);
-        
-        ans.push_back(res);
+          ans.push_back(1); 
+        long long res=1;
+        for(int i=1;i<=rowIndex;i++){
+          res = res *(rowIndex-i+1);
+          res = res / i;
+          ans.push_back(res);
         }
+         
         return ans;
     }
 
-    int returnelement(int row,int column){
-     long long res=1;
-     for(int i=0;i<column;i++){
-       res = res * (row-i);
-       res = res / (i+1);
-       
-     }
-     return res;
-    }
+
 };
