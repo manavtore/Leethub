acc570f281b8634211f36a041d1068512b6a6183
manavtore <113475112+manavtore@users.noreply.github.com>
2023-08-31 23:34:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (69.92%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+       
+        int maximum=0;
+        for(int i=0;i<accounts.size();i++){
+             int sum=0;
+            for(int j=0;j<accounts[i].size();j++){
+            
+            sum += accounts[i][j];
+
+            }
+          maximum = max(sum,maximum);
+        }
+        return maximum;
+    }
+};
