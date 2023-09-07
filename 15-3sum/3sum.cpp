ea70ea00c0350c3e1659eef014ab3a5164db3858
manavtore <113475112+manavtore@users.noreply.github.com>
2023-09-07 23:49:18 +0530

    Time: 80 ms (97.66%) | Memory: 24.1 MB (72.71%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,32 +5,30 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+        for (int i = 0; i < n - 2; i++) {
+            // Skip duplicates of the first element.
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;  
+            int left = i + 1; // Corrected initialization
             int right = n - 1;
             int target = -nums[i];
 
-            while(left < right) {
+            while (left < right) {
                 int sum = nums[left] + nums[right];
 
-                if(sum == target) {
+                if (sum == target) {
                     final.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-                    while(left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1])right--; 
                     left++;
                     right--;
-                } else if(sum > target) {
-                    right--;
+                } else if (sum < target) {
+                    left++;
                 } else {
-                    left++;
+                    right--;
                 }
             }
         }
-
         return final;
     }
 };
