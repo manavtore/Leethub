fcad8f3efbc23153f41e7f39670b1ddbe7b5e30e
manavtore <113475112+manavtore@users.noreply.github.com>
2023-09-21 18:25:49 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (15.49%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        for(int i=s.length()-1;i>=0;i--){
+             if(s[i]!=' '){
+               length++;
+            }else if(length>0){
+                 break;
+            }   
+        }
+        return length;
+    }
+};
