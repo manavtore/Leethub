d308e5dbf9ecda13a863ab769bf1b407a125bc78
manavtore <113475112+manavtore@users.noreply.github.com>
2023-10-27 16:14:13 +0530

    Time: 8 ms (32.65%) | Memory: 7.5 MB (34.09%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.cpp
+++ 205-isomorphic-strings/isomorphic-strings.cpp
@@ -11,51 +11,24 @@ public:
           char schar=s[i];
            char tchar=t[i];
 
-        if(S.find(schar)==S.end()){
+        if(S.find(schar)!=S.end()){
+            if (S[schar] != tchar) {
+                 return false;   
+            }
+        }else{
             S[schar]=tchar;
-        }else if(S[schar]!=tchar){
-            return false;
         }
 
-         if(T.find(tchar)==T.end()){
+         if(T.find(tchar)!=T.end()){
+             if(T[tchar] != schar){
+                 return false;
+             }
+         }else{
              T[tchar]=schar;
-         }else if(T[tchar]!=schar){
-             return false;
-         }
-           
-           
+         }        
 
     }
      return true;
     }
 };
 
-// class Solution {
-// public:
-//     bool isIsomorphic(string s, string t) {
-//         if (s.length() != t.length()) {
-//             return false;
-//         }
-//         unordered_map<char, char> S;
-//         unordered_map<char, char> T;
-
-//         for (int i = 0; i < s.length(); i++) {
-//             char schar = s[i];
-//             char tchar = t[i];
-
-//             if (S.find(schar) == S.end()) {
-//                 S[schar] = tchar;
-//             } else if (S[schar] != tchar) {
-//                 return false;
-//             }
-
-//             if (T.find(tchar) == T.end()) {
-//                 T[tchar] = schar;
-//             } else if (T[tchar] != schar) {
-//                 return false;
-//             }
-//         }
-
-//         return true;
-//     }
-// };
