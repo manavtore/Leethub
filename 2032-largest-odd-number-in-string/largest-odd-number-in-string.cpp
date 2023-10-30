8d13d42d6a43787fd25c237f3a0e7bb7bd635f75
manavtore <113475112+manavtore@users.noreply.github.com>
2023-10-30 23:44:43 +0530

    Time: 28 ms (64.00%) | Memory: 15.2 MB (53.25%) - LeetSync

---
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        
+     int i=num.length()-1;
+     while(i>=0 && num[i]%2==0){
+         i--;
+     }
+      if(i<0){
+          return "";
+      }
+       return num.substr(0,i+1);
+    }
+};
