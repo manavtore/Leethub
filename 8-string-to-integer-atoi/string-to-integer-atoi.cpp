b590a70eccce6dd2a99cd55d951087e52d9ae992
manavtore <113475112+manavtore@users.noreply.github.com>
2023-11-01 20:04:54 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (14.33%) - LeetSync

---
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int myAtoi(string s) {
+        if(s.length()==0) return 0;
+         
+         int i=0;
+         while(i<s.length() && s[i]== ' '){
+         i++;
+         }
+         s = s.substr(i);
+         int sign = 1;
+         long long ans =0;
+         if(s[0]=='-') sign = -1;
+
+        i = (s[0]=='-' || s[0]=='+') ? 1: 0;
+
+          while(i<s.length()){
+              if(!isdigit(s[i]) ) break; 
+
+              ans = ans * 10 + s[i]-'0';
+
+               if (sign == -1 && -1 * ans < INT_MIN) return INT_MIN;
+            if (sign == 1 && ans > INT_MAX) return INT_MAX;
+
+              i++;
+          }
+        
+       return (int)(sign*ans);
+          
+        
+    }
+};
