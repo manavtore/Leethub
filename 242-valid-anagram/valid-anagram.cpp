f4f19d041a9f83b0c56de11e13bba678b7aa8372
manavtore <113475112+manavtore@users.noreply.github.com>
2023-12-16 09:53:29 +0530

    Time: 6 ms (80.64%) | Memory: 7.9 MB (20.94%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,8 +1,22 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       sort(s.begin(),s.end());
-       sort(t.begin(),t.end());
-       return s==t;
+        if(s.length()!=t.length()) return false;
+
+        unordered_map<char,int> charCount;
+
+          for (char c : s) {
+            charCount[c]++;
+              }
+         for (char c : t) {
+            charCount[c]--;
+              }
+    
+          for(const auto& pair:charCount){
+              if(pair.second!=0){
+                  return false;
+              }
+          }
+       return true;
     }
 };
