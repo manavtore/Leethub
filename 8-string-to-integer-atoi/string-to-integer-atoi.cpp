993a5ec9c0a4a13209591c5e620b3e78267280f7
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-12 14:10:34 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (7.32%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -12,20 +12,19 @@ public:
         int sign=1;
         long long ans=0;
         if(s[0]=='-') sign=-1;
-
-        i = (s[0]=='-' || s[0]=='+') ? 1:0;
-
-        while(i<s.length()){
-            if(!isdigit(s[i])) break;
+       return recursiveString(s,i = (s[0]=='-' || s[0]=='+') ? 1:0,sign,ans);
+        
+    }
+    int recursiveString(string s,int i,int sign,long long& ans){
+         if(i==s.length() || !isdigit(s[i])){
+             return (int)(sign * ans);
+         }
             
             ans = ans * 10 +s[i]-'0';
 
             if(sign==-1 && -1*ans <INT_MIN) return INT_MIN;
             if(sign== 1 && ans > INT_MAX) return INT_MAX;
 
-            i++;
-        }
-        return (int)(sign*ans);
-        
+            return recursiveString(s,i+1,sign,ans);
     }
 };
