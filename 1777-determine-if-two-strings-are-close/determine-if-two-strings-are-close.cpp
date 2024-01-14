3dbaa2067ea1171e6e0da5cc81e934c6f421b57a
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-14 19:19:38 +0530

    Time: 69 ms (86.05%) | Memory: 21 MB (78.40%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        int n=word1.length();
+        int m=word2.length();
+        if(n!=m) return false;
+        vector<int> freq1(26,0);
+        vector<int> freq2(26,0);
+        for(int i=0;i<n;i++){
+            freq1[word1[i]-'a']++;
+            freq2[word2[i]-'a']++;
+        }
+       for(int i=0;i<26;i++){
+           if( (freq1[i]!=0 && freq2[i]==0) || (freq2[i]!=0 && freq1[i]==0)){
+               return false;
+           }
+       }
+       sort(freq1.begin(),freq1.end());
+       sort(freq2.begin(),freq2.end());
+
+       for(int i=0;i<26;i++){
+           if(freq1[i]!=freq2[i]){
+               return false;
+           }
+       }
+        return true;
+    }
+};
