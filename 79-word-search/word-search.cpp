07bf1e14ac088abd33125a4ecbc87fbea0d4ef2f
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-18 19:23:18 +0530

    Time: 290 ms (85.20%) | Memory: 8.4 MB (76.23%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,38 +1,42 @@
 class Solution {
 public:
-    bool exist(vector<vector<char>>& board, string word) {
-       int Rows = board.size();
-       int column = board[0].size();
+    bool check(vector<vector<char>>& board,string& word,int i,int j,int index){
+        if (index == word.length()) {
+            return true;
+            }
+            int m=board.size();
+            int n=board[0].size();
 
-       for(int i=0;i<Rows;i++){
-           for(int j=0;j<column;j++){
-               if(searchword(board,word,i,j,0)){
-                   return true;     
-               }
-           }
-       }
-          return false;
-        
-    }
-    bool searchword(vector<vector<char>>& board, string word,int i,int j,int index){
-           if(index == word.length()){
-                   return true;
-               }
-          if(i<0 || i>=board.size() || j<0 || j>=board[0].size() || board[i][j]!= word[index]){
-                   return false ;
-               }
 
-        char currentchar = board[i][j];
-        board[i][j] = ' '; 
+            if(i<0 || i>=m || j<0 || j>=n || word[index]!=board[i][j]){
+                return false;
+            }
+             char temp=board[i][j];
+             board[i][j]='#';
 
-        bool found = searchword(board,word,i-1,j,index+1) ||
-                     searchword(board,word,i+1,j,index+1) ||
-                     searchword(board,word,i,j+1,index+1) ||
-                     searchword(board,word,i,j-1,index+1) ;
+      
+              bool found =  check(board, word, i + 1, j, index + 1) ||
+                check(board, word, i, j + 1, index + 1) ||
+                check(board, word, i, j - 1, index + 1) ||
+                check(board, word, i - 1, j, index + 1);
+       
+                
+                board[i][j]=temp;
 
-        board[i][j] = currentchar;
-
-        return found;        
+            return found;
+            }
+            
     
+    bool exist(vector<vector<char>>& board, string word) {
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(check(board,word,i,j,0)){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
