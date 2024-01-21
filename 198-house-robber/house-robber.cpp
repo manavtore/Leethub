c5c71d5f66a362401021c11589abbc87d24af4b9
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-21 13:58:53 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (9.67%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -6,12 +6,13 @@ public:
         int n=nums.size();
         if(n==0) return 0;
         if(n==1) return nums[0];
-        int dp[n+1];
-        dp[0]=nums[0];
-        dp[1]=max(nums[0],nums[1]);
+        int prev1=nums[0];
+        int prev2=max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
-           dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+           int current = max(prev2,prev1+nums[i]);
+           prev1=prev2;
+           prev2=current;
         }
-        return dp[n-1];
+        return prev2;
     }
 };
