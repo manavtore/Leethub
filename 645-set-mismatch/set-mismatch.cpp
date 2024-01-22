7d32f9d911ffc4a2074a2a403e55d1cef03c95e7
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-22 10:30:29 +0530

    Time: 1747 ms (5.05%) | Memory: 23.8 MB (36.81%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int missing=-1;
+        int dup=-1;
+
+        for(int i=1;i<=nums.size();i++){
+            int count=0;
+            for(int j=0;j<nums.size();j++){
+                if(nums[j]==i){
+                    count++;
+                }
+               
+            }
+             if(count==2){
+                   dup=i;
+                }else if(count==0){
+                    missing=i;
+                }
+        }
+        return {dup,missing};
+        
+    }
+};
