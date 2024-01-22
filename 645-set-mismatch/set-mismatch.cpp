239b86666c7ccae25fca86559753b08ed491f40e
manavtore <113475112+manavtore@users.noreply.github.com>
2024-01-22 10:57:04 +0530

    Time: 23 ms (73.89%) | Memory: 24.7 MB (34.82%) - LeetSync

--- 645-set-mismatch/set-mismatch.cpp
+++ 645-set-mismatch/set-mismatch.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
-        int missing=-1;
-        int dup=-1;
+        int n=nums.size();
+        int missing=0;
+        int duplicate=0;
+        vector<int> freq(n+1,0);
 
-        for(int i=1;i<=nums.size();i++){
-            int count=0;
-            for(int j=0;j<nums.size();j++){
-                if(nums[j]==i){
-                    count++;
+
+        for(int i=0;i<n;i++){
+            freq[nums[i]]++;
                 }
-               
-            }
-             if(count==2){
-                   dup=i;
-                }else if(count==0){
-                    missing=i;
-                }
+        for(int i=1;i<freq.size();i++){
+            if(freq[i]==2) duplicate=i;
+            if(freq[i]==0) missing=i;
         }
-        return {dup,missing};
+        return {duplicate,missing};
         
     }
 };
