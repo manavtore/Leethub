3d59a7045efc87ad047c5367197338cc7c178a52
manavtore <113475112+manavtore@users.noreply.github.com>
2024-02-06 00:43:18 +0530

    Time: 15 ms (85.23%) | Memory: 21 MB (17.95%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,34 +1,47 @@
 class MinStack {
-    stack<int> mainstack;
-    stack<int> minstack;
+    stack<long long> st;
+    long long mini;
 
 public:
     MinStack() {
+        while(st.empty()==false) st.pop();
+        mini=INT_MAX;
     }
     
-    void push(int val) {
-        mainstack.push(val);
-        if(minstack.empty() || val<=minstack.top()){
-            minstack.push(val);
+    void push(int value) {
+         long long val = static_cast<long long>(value);
+        if(st.empty()){
+            mini=val;
+            st.push(val);
+        }else{
+            if(val<mini){
+                st.push(2*val*1LL-mini);
+                mini=val;
+            }else{
+                st.push(val);
+            }
         }
     }
     
     void pop() {
-        if(!mainstack.empty()){
-            if(mainstack.top()==minstack.top()){
-                minstack.pop();
-            }
-            mainstack.pop();
-
+        if(st.empty()) return ;
+        long long el=st.top();
+        st.pop();
+        if(el<mini){
+           mini = 2 * mini - el;
         }
     }
     
     int top() {
-           return mainstack.top();
+          if(st.empty()) return -1;
+
+          long long el =st.top();
+          if(el<mini) return mini;
+          return el;
     }
     
     int getMin() {
-            return minstack.top();
+            return  mini;
     }
 };
 
