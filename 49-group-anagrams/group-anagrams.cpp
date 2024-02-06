327c4193a541c3251bbbb7c506e4f9c60e965c8a
manavtore <113475112+manavtore@users.noreply.github.com>
2024-02-06 11:21:28 +0530

    Time: 42 ms (24.69%) | Memory: 31.6 MB (6.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -3,14 +3,20 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         map<string,vector<string>> group;
         for(const auto& str:strs){
-            string original=str;
-            sort(original.begin(),original.end());
-            group[original].push_back(str);
+            vector<int> freq(26,0);
+            string hashStr="";
+            for(auto s:str) freq[s-'a']++;
+            for(int i=0;i<26;i++){
+                hashStr.push_back(freq[i]);
+                hashStr.push_back('#');
+            }
+           group[hashStr].push_back(str);
         }
         vector<vector<string>> anagrams;
         for(const auto& pair :group){
             anagrams.push_back(pair.second);
         }   
         return anagrams;
+       
     }
 };
