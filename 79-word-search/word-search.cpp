5934d48277c86fafa8d5dbf58df1ac4d93959dbc
manavtore <113475112+manavtore@users.noreply.github.com>
2024-04-03 23:36:02 +0530

    Time: 1056 ms (16.95%) | Memory: 11.7 MB (49.58%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    bool check(vector<vector<char>>& board,string& word,int i,int j,int index){
-        if (index == word.length()) {
+    bool check(vector<vector<char>>& board,int i,int j,int index,string word){
+        if(index==word.length()){
             return true;
-            }
-            int m=board.size();
-            int n=board[0].size();
+        }
+        int n=board.size();
+        int m=board[0].size();
 
+        if(i<0 || i>=n || j<0 || j>=m || word[index]!=board[i][j]){
+            return false;
+        }
+        char temp=board[i][j];
+        board[i][j]='#';
 
-            if(i<0 || i>=m || j<0 || j>=n || word[index]!=board[i][j]){
-                return false;
-            }
-             char temp=board[i][j];
-             board[i][j]='#';
+        bool found = check(board,i+1,j,index+1,word) ||
+                     check(board,i,j+1,index+1,word) ||
+                     check(board,i,j-1,index+1,word) ||
+                     check(board,i-1,j,index+1,word) ;
 
-      
-              bool found =  check(board, word, i + 1, j, index + 1) ||
-                check(board, word, i, j + 1, index + 1) ||
-                check(board, word, i, j - 1, index + 1) ||
-                check(board, word, i - 1, j, index + 1);
-       
-                
-                board[i][j]=temp;
-
-            return found;
-            }
-            
-    
+         board[i][j]=temp;
+         return found;
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(check(board,word,i,j,0)){
+        int n = board.size();
+        int m = board[0].size();
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(check(board,i,j,0,word)){
                     return true;
                 }
             }
