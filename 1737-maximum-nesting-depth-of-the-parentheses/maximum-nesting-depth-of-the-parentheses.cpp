dbcffef5280cc61c255a916c4a7e33d9653683e3
manavtore <113475112+manavtore@users.noreply.github.com>
2024-04-04 17:40:15 +0530

    Time: 0 ms (100.00%) | Memory: 7.4 MB (49.50%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -3,14 +3,14 @@ public:
     int maxDepth(string s) {
         int maximum =0;
         int depth =0;
-        for(char c: s){
-           if(c == '(' ){
-              depth++;
-              maximum = max(depth,maximum);
-           }else if(c == ')'){
-              depth--;
-           }
+        for(char c : s){
+            if(c=='('){
+                depth++;
+                maximum = max(depth,maximum);
+            }else if(c == ')'){
+                depth--;
+            }
         }
-     return maximum;
+        return maximum;
     }
 };
