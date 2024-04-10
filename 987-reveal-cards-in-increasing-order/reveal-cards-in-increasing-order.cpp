611030fd03b7c1b454c95b2a9b6b426fa7da4ced
manavtore <113475112+manavtore@users.noreply.github.com>
2024-04-11 04:53:33 +0530

    Time: 6 ms (30.84%) | Memory: 10.7 MB (91.48%) - LeetSync

---
+++ 987-reveal-cards-in-increasing-order/reveal-cards-in-increasing-order.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        queue<int> q;
+        int n = deck.size();
+        for(int i=0;i<n;i++){
+            q.push(i);
+        }
+        sort(deck.begin(),deck.end());
+
+        vector<int> ans(n);
+        for(int i=0;i<n;i++){
+            ans[q.front()]=deck[i];
+            q.pop();
+            if(!q.empty()){
+                q.push(q.front());
+                q.pop();
+        }
+        }
+        return ans;
+        
+    }
+};
