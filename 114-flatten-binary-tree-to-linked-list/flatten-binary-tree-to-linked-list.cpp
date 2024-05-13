51755f74e0059cdd98052d6993815984c4011947
manavtore <113475112+manavtore@users.noreply.github.com>
2024-05-13 16:07:11 +0530

    Time: 3 ms (77.27%) | Memory: 16.1 MB (8.01%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -10,16 +10,24 @@
  * };
  */
 class Solution {
-    TreeNode* prev = nullptr;
 public:
     void flatten(TreeNode* root) {
-        if(root==nullptr) return;
-
-        flatten(root->right);
-        flatten(root->left);
-
-        root->right = prev;
-        root->left = nullptr;
-        prev = root;
+        if(root==nullptr) return ;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* curr= st.top();
+            st.pop();
+            if(curr->right!=nullptr){
+                st.push(curr->right);
+            }
+            if(curr->left!=nullptr){
+                st.push(curr->left);
+            }
+            if(!st.empty()){
+                curr->right = st.top();
+            }
+            curr->left = nullptr;
+        }
     }
 };
