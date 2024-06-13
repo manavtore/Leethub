dafed20416d1b70649aa139c8d61e10efac142b2
manavtore <113475112+manavtore@users.noreply.github.com>
2024-06-13 16:38:01 +0530

    Time: 7 ms (60.18%) | Memory: 21.1 MB (17.35%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(),seats.end());
-        sort(students.begin(),students.end());
-        int move =0;
-        for(int i=0;i<students.size();i++){
-            if(seats[i]!=students[i]){
-                move +=abs(seats[i]-students[i]);
-            }
+        vector<int> seat(*max_element(seats.begin(), seats.end()) + 1, 0);
+        vector<int> student(*max_element(students.begin(), students.end()) + 1, 0);
+
+        for(auto el:seats) seat[el]++;
+        for(auto el:students) student[el]++;
+        int j=0,ans=0;
+        for(int i=0;i<seat.size();i++){
+            if(seat[i]==0) continue;
+            while(student[j]==0) j++;
+            ans += abs(i-j);
+            student[j]--;
+            seat[i]--;
+            if(seat[i]!=0) i--; 
         }
-        return move;
+        return ans;
     }
 };
