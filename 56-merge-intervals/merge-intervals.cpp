ff398f519693a9bcd7f94291fa7663828cb8c596
manavtore <113475112+manavtore@users.noreply.github.com>
2024-06-17 12:35:03 +0530

    Time: 24 ms (50.57%) | Memory: 22.6 MB (68.03%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        if (intervals.empty()) {
+        if(intervals.empty()){
             return {};
         }
+        sort(intervals.begin(),intervals.end());
 
-        sort(intervals.begin(), intervals.end());
-            
         vector<vector<int>> result;
         int start = intervals[0][0];
-         int end = intervals[0][1];
+        int end = intervals[0][1];
+
         for(int i=1;i<intervals.size();i++){
-            
             if(intervals[i][0]<=end){
-                end=max(end,intervals[i][1]);
+                end = max(end,intervals[i][1]);
             }else{
                 result.push_back({start,end});
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
-            
-            }
-            
-            result.push_back({start,end});
-
-            return result;
-          
-
-            
-
-
-        
+        }
+        result.push_back({start,end});
+        return result;
     }
 };
