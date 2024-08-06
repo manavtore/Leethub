cd0101991db834df368d8fab3ca0ca078de20884
manavtore <113475112+manavtore@users.noreply.github.com>
2024-08-07 00:58:21 +0530

    Time: 58 ms (97.22%) | Memory: 27.7 MB (17.31%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        vector<int> freq(26,0);
-        for(int i=0;i<word.length();i++){
-            freq[word[i]-'a']++;
+        vector<int> freq(26, 0);
+
+        for (char ch : word) {
+            freq[ch - 'a']++;
         }
-        sort(freq.rbegin(),freq.rend());
-        int push=1;
-        int cnt=1;
-        int ans=0;
-        for(int i=0;i<26;i++){
-            if(freq[i]==0) break;
-            ans += freq[i]*push;
-            if(cnt%8==0){
+
+        sort(freq.rbegin(), freq.rend());
+
+        int push = 1;
+        int ans = 0;
+
+        for (int i = 0, cnt = 1; i < 26; i++) {
+            if (freq[i] == 0) break;
+            ans += freq[i] * push;
+
+            if (cnt % 8 == 0) {
                 push++;
             }
             cnt++;
-
         }
-        
+
         return ans;
     }
 };
