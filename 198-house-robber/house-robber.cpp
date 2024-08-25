ce9189232dc036ca40b8af82ca197c44af33f116
manavtore <113475112+manavtore@users.noreply.github.com>
2024-08-25 11:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (36.23%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,18 @@
 class Solution {
-public:
-    
-    int rob(vector<int>& nums) {
+    int robSecure(vector<int>& nums,vector<int>& dp,int ind){
+        if(ind==0) return nums[ind];
+        if(ind<=0) return 0;
 
-        int n=nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        int prev1=nums[0];
-        int prev2=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-           int current = max(prev2,prev1+nums[i]);
-           prev1=prev2;
-           prev2=current;
-        }
-        return prev2;
+        if(dp[ind]!=-1) return dp[ind];
+
+        int left = robSecure(nums,dp,ind-2)  + nums[ind];
+        int right = 0 + robSecure(nums,dp,ind-1);
+        return dp[ind] = max(left,right);
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n,-1);
+        return robSecure(nums,dp,n-1);
     }
 };
