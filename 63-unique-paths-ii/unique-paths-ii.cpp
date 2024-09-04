629d72f6a49c0fa5d9d5bc2f1afd6ee1b0a9bdd0
manavtore <113475112+manavtore@users.noreply.github.com>
2024-09-05 04:37:57 +0530

    Time: 3 ms (53.37%) | Memory: 10.6 MB (6.61%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,20 +1,31 @@
 class Solution {
-    int unique(vector<vector<int>>& obs,int n,int m,vector<vector<int>>& dp){
-        if (n < 0 || m < 0) return 0;
-        if(obs[n][m]==1) return 0;
-        if(n==0 && m==0)return 1;
-        
-         if(dp[n][m]!=-1) return dp[n][m];
-        int up = unique(obs,n-1,m,dp);
-        int left = unique(obs,n,m-1,dp);
-
-        return dp[n][m] = up + left;
-    }
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs) {
-        int m = obs.size();
-        int n = obs[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        return unique(obs,m-1,n-1,dp);
+        int n = obs.size();
+        int m = obs[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+        if (obs[0][0] == 1) return 0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(obs[i][j]==1){
+                    dp[i][j]=0;
+                    continue;
+                }
+
+                if(i==0 && j==0){
+                    dp[i][j]=1;
+                    continue;
+                }
+                int up = 0;
+                int left = 0;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+
+                dp[i][j] = up + left;
+                
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
