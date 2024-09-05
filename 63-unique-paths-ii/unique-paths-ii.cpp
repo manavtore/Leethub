366aa28f9f71e42f6670a2218bfe6c4fbf7a0537
manavtore <113475112+manavtore@users.noreply.github.com>
2024-09-05 06:35:18 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (76.05%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -3,29 +3,34 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obs) {
         int n = obs.size();
         int m = obs[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        if (obs[0][0] == 1) return 0;
+        vector<int> dp(m,-1);
         for(int i=0;i<n;i++){
+            vector<int> temp(m,0);
             for(int j=0;j<m;j++){
 
                 if(obs[i][j]==1){
-                    dp[i][j]=0;
+                    temp[j]=0;
                     continue;
                 }
 
                 if(i==0 && j==0){
-                    dp[i][j]=1;
+                    temp[j]=1;
                     continue;
-                }
+                }else{
                 int up = 0;
                 int left = 0;
-                if(i>0) up = dp[i-1][j];
-                if(j>0) left = dp[i][j-1];
+                if(i>0){
+                up = dp[j];
+                }
+                if(j>0){
+                left = temp[j-1];
+                }
 
-                dp[i][j] = up + left;
-                
+                temp[j] = up + left;
+                }
             }
+            dp = temp;
         }
-        return dp[n-1][m-1];
+        return dp[m-1];
     }
 };
