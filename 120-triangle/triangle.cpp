84a17569922f3c3567a939509e10df2ec82a62c0
manavtore <113475112+manavtore@users.noreply.github.com>
2024-09-18 21:02:13 +0530

    Time: 8 ms (27.86%) | Memory: 11.6 MB (71.30%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,19 +1,24 @@
 class Solution {
-    int minimum(int i,int j,vector<vector<int>>& triangle,vector<vector<int>>& dp){
-        int n = triangle.size();
-        if(i==n-1) return triangle[i][j];
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int down = triangle[i][j] + minimum(i+1,j,triangle,dp);
-        int diagonal = triangle[i][j] + minimum(i+1,j+1,triangle,dp);
-
-        return dp[i][j] = min(down,diagonal);
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(triangle[n-1].size(),-1));
-        return minimum(0,0,triangle,dp);
+        vector<vector<int>> dp(n,vector<int> (triangle[n-1].size(),-1));
+
+        for(int j=n-1;j>=0;j--){
+            dp[n-1][j] = triangle[n-1][j];
+        }
+
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+
+                int down = triangle[i][j] + dp[i+1][j];
+                int side = triangle[i][j] + dp[i+1][j+1];
+
+                dp[i][j] = min(down,side);
+            }
+        }
+
+        return dp[0][0];
+
     }
 };
