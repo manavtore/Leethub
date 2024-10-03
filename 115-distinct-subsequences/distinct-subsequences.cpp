b777269e458424ba842d068c924bfd1f97a1b9de
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-03 13:10:49 +0530

    Time: 20 ms (55.92%) | Memory: 43 MB (22.11%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,26 +1,25 @@
 class Solution {
-    int calculateDistinct(string s,string t, int i, int j,vector<vector<int>>& dp){
-        if( j < 0 ) return 1;
-
-        if( i < 0 ) return 0;
-
-        if( dp[i][j] !=  -1 ) return dp[i][j];
-
-        if( s[i] == t[j] ){
-            return dp[i][j] = calculateDistinct(s,t,i-1,j-1,dp) + calculateDistinct(s,t,i-1,j,dp);
-        }
-
-            return dp[i][j] = calculateDistinct(s,t,i-1,j,dp);
-
-    }
 public:
     int numDistinct(string s, string t) {
-
         int n = s.length();
         int m = t.length();
 
-        vector<vector<int>> dp(n,vector<int>(m,-1));
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
 
-        return calculateDistinct(s,t,n-1,m-1,dp);
+        for(int i = 0; i <= n; i++ ) dp[i][0] = 1;
+
+
+        for(int i=1 ; i<=n ;i++){
+            for(int j=1; j<=m; j++){
+                if( s[i-1] == t[j-1] ){
+                   dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+               }else{
+                 dp[i][j] = dp[i-1][j];
+               }
+
+            }
+        }
+
+        return (int)dp[n][m];
     }
 };
