3ae48128c4ed79ab8a3a476f057aba791030d4cc
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-07 11:57:01 +0530

    Time: 425 ms (51.79%) | Memory: 210.1 MB (42.68%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,22 +1,4 @@
 class Solution {
-    int buyandsell(vector<int>& prices,int ind,int buy,int cap, vector<vector<vector<int>>>& dp){
-        if(cap == 0) return 0;
-        if(ind == prices.size()) return 0;
-        if(dp[ind][buy][cap] != -1) return dp[ind][buy][cap];
-        int profit = 0;
-        if(buy){
-            profit = max(
-                        -prices[ind] + buyandsell(prices,ind+1,0,cap,dp),
-                        0 + buyandsell(prices,ind+1,1,cap,dp)
-            );
-        }else{
-             profit = max(
-                        prices[ind] + buyandsell(prices,ind+1,1,cap-1,dp),
-                        buyandsell(prices,ind+1,0,cap,dp)
-                         );
-        }
-        return dp[ind][buy][cap] = profit;
-    }
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
@@ -29,14 +11,12 @@ public:
                  int profit = 0;
                  if(buy){
                         profit = max(
-                        -prices[ind] + buyandsell(prices,ind+1,0,cap,dp),
-                        0 + buyandsell(prices,ind+1,1,cap,dp)
-                        );
+                        -prices[ind] + dp[ind+1][0][cap],
+                        0 + dp[ind+1][1][cap]);
                     }else{
                         profit = max(
-                        prices[ind] + buyandsell(prices,ind+1,1,cap-1,dp),
-                        buyandsell(prices,ind+1,0,cap,dp)
-                         );
+                        prices[ind] + dp[ind+1][1][cap-1],
+                        dp[ind+1][0][cap]);
                   }
                   dp[ind][buy][cap] = profit;
                 }
