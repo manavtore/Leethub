eaee58dc583b89b67fb1e0fe9aa4517526c59ac9
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-09 23:31:41 +0530

    Time: 12 ms (83.63%) | Memory: 11.6 MB (69.01%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.cpp
+++ 368-largest-divisible-subset/largest-divisible-subset.cpp
@@ -1,32 +1,37 @@
 class Solution {
 public:
     vector<int> largestDivisibleSubset(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        vector<int> dp(nums.size(), 1);
-        vector<int> prev(nums.size(), -1); 
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
 
-        int maxSize = 0; 
-        int maxIndex = -1;
+        vector<int> dp(n+1,0);
+        vector<int> hash(n,0);
+        iota(hash.begin(),hash.end(),0);
 
-        for (int i = 0; i < nums.size(); ++i) {
-            for (int j = 0; j < i; ++j) {
-                if (nums[i] % nums[j] == 0 && dp[i] < dp[j] + 1) {
+        int maxlength = 0;
+        int lastIndex = 0;
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]%nums[j]==0 && dp[j]+1 >dp[i]){
                     dp[i] = dp[j] + 1;
-                    prev[i] = j;
+                    hash[i] = j;
                 }
             }
-            if (dp[i] > maxSize) {
-                maxSize = dp[i];
-                maxIndex = i;
+            if(dp[i] > maxlength){
+                maxlength = dp[i];
+                lastIndex = i;
             }
         }
 
-        vector<int> result;
-        while (maxIndex != -1) {
-            result.push_back(nums[maxIndex]);
-            maxIndex = prev[maxIndex];
-        }
+        vector<int> ans;
 
-        return result;
-    }
+        while(lastIndex != hash[lastIndex]){
+            ans.push_back(nums[lastIndex]);
+            lastIndex = hash[lastIndex];
+        }
+        ans.push_back(nums[lastIndex]);
+        sort(ans.begin(),ans.end());
+        return ans;
+    }   
 };
