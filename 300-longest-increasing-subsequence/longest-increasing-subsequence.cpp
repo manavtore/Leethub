5e8e58c60ad4f1310c3163ce6d9122d19459c53c
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-09 10:01:22 +0530

    Time: 65 ms (69.34%) | Memory: 13.1 MB (84.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,19 +1,16 @@
 class Solution {
-    int calculateLength(vector<int>& nums,int ind,int prevind,int n,vector<vector<int>>& dp){
-        if(ind==n) return 0;
-        if(dp[ind][prevind+1]!= -1) return dp[ind][prevind+1];
-
-        int len = calculateLength(nums,ind+1,prevind,n,dp);
-
-        if(prevind == -1 || nums[ind]>nums[prevind]){
-            len = max(len,1 + calculateLength(nums,ind+1,ind,n,dp));
-        }
-        return dp[ind][prevind+1] = len;
-    }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n,vector<int>(n+1,-1));
-        return calculateLength(nums,0,-1,n,dp);
+        vector<int> dp(n,1);
+
+        for(int i=1;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] = max(dp[i],dp[j]+1);
+                }
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
     }
 };
