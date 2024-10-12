9292bf4488d08799a7559170e01d01abbbf50df5
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-12 19:20:41 +0530

    Time: 37 ms (52.32%) | Memory: 20.5 MB (49.43%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -2,29 +2,31 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1,vector<int> (amount+1,INT_MAX));
+        vector<int> prev(amount+1,0);
+        vector<int> curr(amount+1,0);
 
-        for(int i=0;i<n;i++){
-            dp[i][0] = 0;
-        }
+        // for(int i=0;i<n;i++){
+        //     dp[i][0] = 0;
+        // }
 
         for(int ind=0;ind<n;ind++){
             for(int amt = 1;amt<=amount;amt++){
-                 int exclude = (ind>0) ? dp[ind - 1][amt] :INT_MAX;
+                int exclude = (ind>0) ? prev[amt] :INT_MAX;
 
                 int include = INT_MAX;
 
                 if(coins[ind] <= amt){
-                int res = dp[ind][amt-coins[ind]];
+                int res = curr[amt-coins[ind]];
                     if (res != INT_MAX) {
                         include = 1 + res; 
                  }
              }
 
-              dp[ind][amt] = min(exclude,include);
+              curr[amt] = min(exclude,include);
             }
+            prev = curr;
         }
-         int result = dp[n-1][amount];
+         int result = prev[amount];
         
         return (result == INT_MAX) ? -1 : result;
     }
