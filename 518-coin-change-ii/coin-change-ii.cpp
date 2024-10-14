f17c2198d7870fe6a9473c276313db883a9e9e2a
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-14 23:43:36 +0530

    Time: 22 ms (25.36%) | Memory: 26.7 MB (26.03%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -2,24 +2,25 @@ class Solution {
 public:
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n,vector<int>(amount+1,0));
+
+        vector<int> prev(amount+1,0);
         
-        for(int i=0;i<=amount ;i++){
-          if (i % coins[0] == 0)
-               dp[0][i] = 1;
-           }
+        prev[0] = 1;
     
-        for(int ind=1;ind<n;ind++){
-            for(int amt = 0; amt<=amount ; amt++){
-                  long long exclude = dp[ind-1][amt];
+        for(int ind=0;ind<n;ind++){
+            vector<int> curr(amount+1,0);
+            curr[0] = 1; 
+            for(int amt = 1; amt<=amount ; amt++){
+                  long long exclude = prev[amt];
 
                   long long include = 0;
                   if(coins[ind] <= amt){
-                    include = dp[ind][amt- coins[ind]];
+                    include = curr[amt- coins[ind]];
                    }
-                  dp[ind][amt] = include + exclude ;
+                  curr[amt] = include + exclude ;
             }
+            prev = curr;
         }
-        return dp[n-1][amount];
+        return prev[amount];
     }
 };
