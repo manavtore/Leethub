66d02917d8f5249a4d915a4158928c626091e3ad
manavtore <113475112+manavtore@users.noreply.github.com>
2024-10-17 22:56:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (31.77%) - LeetSync

---
+++ 670-maximum-swap/maximum-swap.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string str = to_string(num);
+        int n = str.size();
+        int last[10] = {0};
+        
+        for (int i = 0; i < n; ++i) {
+            last[str[i] - '0'] = i;
+        }
+        
+        for (int i = 0; i < n; ++i) {
+            for (int d = 9; d > str[i] - '0'; --d) {
+                if (last[d] > i) {
+                    swap(str[i], str[last[d]]);
+                    return stoi(str);
+                }
+            }
+        }
+        
+        return num;
+    }
+};
