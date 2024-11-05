8c9e75f32f4fc75d5af06f2ce3bbf2ab37a796ad
manavtore <113475112+manavtore@users.noreply.github.com>
2024-11-05 19:54:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (67.52%) - LeetSync

--- 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
+++ 3174-minimum-number-of-changes-to-make-binary-string-beautiful/minimum-number-of-changes-to-make-binary-string-beautiful.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int minChanges(string s) {
-        int l = 0;
         int cnt = 0;
-
-        for(int r=0;r<s.length();r++){
-            if(s[r] != s[l]){
-                if(r & 1){
-                    cnt++;
-                }
-                l = r;
+        for(int r=0;r<s.length()-1;r+=2){
+            if(s[r] != s[r+1]){
+                cnt++;
             }
         }
 
