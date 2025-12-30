162d510f77dd1445fd62893cbfe2415fd13f84f8
manavtore <113475112+manavtore@users.noreply.github.com>
2025-12-30 14:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (34.65%) - LeetSync

--- 870-magic-squares-in-grid/magic-squares-in-grid.cpp
+++ 870-magic-squares-in-grid/magic-squares-in-grid.cpp
@@ -1,13 +1,11 @@
 class Solution {
-private:
-    bool magicQuid(vector<vector<int>>& grid,int row,int col){
-
+    bool MagicQuad(vector<vector<int>>& grid,int row,int col){
         set<int> st;
 
-        for(int i=row;i<=row+2;i++){
-            for(int j=col;j<=col+2;j++){
+        for(int i = row;i <= row + 2;i++){
+            for(int j = col;j <= col + 2;j++ ){
                 int num = grid[i][j];
-                if(num<1 || num>9 || st.find(num)!=st.end() ){
+                if(num<1 || num>9 || st.find(num)!=st.end()){
                     return false;
                 }
                 st.insert(num);
@@ -25,21 +23,23 @@ private:
         int sum7 = grid[row][col] + grid[row+1][col+1] + grid[row+2][col+2];
         int sum8 = grid[row+2][col] + grid[row+1][col+1] + grid[row][col+2];
 
-        return sum1 == 15 && sum2==15 && sum3==15 && sum4==15 && sum5==15 && sum6==15 && sum7==15 &&  sum8==15;
+        return sum1==15 && sum2==15 && sum3==15 && sum4==15 && sum5==15 && sum6==15 && sum7==15 && sum8==15;
     }
 public:
     int numMagicSquaresInside(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        int cnt =0;
+        
+        int cnt = 0;
+
         for(int i=0;i<=row-3;i++){
             for(int j=0;j<=col-3;j++){
-                if(magicQuid(grid,i,j)){
+                if(MagicQuad(grid,i,j)){
                     cnt++;
                 }
             }
         }
         return cnt;
+
     }
 };
-
