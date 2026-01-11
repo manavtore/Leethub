8ecf204f3a304c083d508a2a321868dfd1a2756e
manavtore <113475112+manavtore@users.noreply.github.com>
2026-01-11 19:29:30 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (13.31%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -15,9 +15,7 @@ public:
         if(p==nullptr && q==nullptr) return true;
         if(p==nullptr || q==nullptr) return false;
 
-        if(p->val==q->val){
-            return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
-        }
-        return false;
+        if(p->val != q->val) return false;
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
     }
 };
