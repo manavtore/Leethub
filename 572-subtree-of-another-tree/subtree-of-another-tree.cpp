75867a8eecc5cb40c4f385962bce837bee9596ed
manavtore <113475112+manavtore@users.noreply.github.com>
2026-01-12 18:12:22 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (70.47%) - LeetSync

---
+++ 572-subtree-of-another-tree/subtree-of-another-tree.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isidentical(TreeNode* root,TreeNode* subRoot){
+        if(root==NULL && subRoot==NULL) return true;
+
+        if(root==NULL || subRoot==NULL) return false;
+
+        if(root->val != subRoot->val){
+            return false;
+        }
+        return isidentical(root->left,subRoot->left) && isidentical(root->right,subRoot->right); 
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root==NULL) return false;
+
+        if(isidentical(root,subRoot)) return true;
+
+        return isSubtree(root->left,subRoot) || isSubtree(root->right,subRoot);
+    }
+};
